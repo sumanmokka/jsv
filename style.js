e6b1f16b80b0c63d3aa31a11f6d207f5245c19ed
sumanmokka <sumanmokka4@gmail.com>
2026-03-11 17:40:32 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,125 @@
+var a="25";
+    a="30";
+console.log(a,)
+let b='suman'
+    b="suman babu"
+console.log(b)
+
+const c="1994"
+console.log(c)
+var a='10';
+var a='20';
+
+console.log(a)
+let d='hollo'
+let d='ramu'// it will be synex error
+
+var e='suman'
+console.log('my name is :-',e)
+var e='29';
+console.log('my age is :-',e)
+var e='hyd';
+console.log('my cicy',e)
+console.log('java script fun')
+
+alert()
+ confirm()
+ prompt()
+ let d='holl'
+ console.warn(d)
+ console.error(d)
+ console.clear(d)
+//document.writeln('hi thi suman')
+ 
+
+let l=prompt('what is you name')
+let p=alert()
+console.log(l)
+
+
+var a=prompt('your age')
+console.log(a)
+document.writeln(a)
+
+const r=confirm('had u brak fost')
+console.log(r)
+var city='kavali'
+var country='india'
+var plant='earth'
+console.log(city)
+console.log(country)
+console.log(plant)
+
+let g='5'
+console.log(g)
+     g='10'
+     console.log(g)
+     g='15'
+     console.log(g)
+
+console.log('halo')
+console.warn('holl')
+ console.error('holl')
+ console.clear('hlokk')
+
+
+ const v=prompt('name')
+let m=prompt()
+ var n=confirm('are you student')
+ alert('Wl come')
+
+
+ console.clear()
+ alert('hallo')
+var x= prompt('what is your name')
+alert('wlacocom')
+document.writeln(x)
+
+//javvascript{     //Unexpected token ' erro
+
+    //var a=100
+    //let b=200
+    //const c=300
+
+//}
+console.log(a)
+console.log(b)
+console.log(c)
+
+var u=10
+let q=20
+const z=30
+console.log(u,u,u)
+
+var a=confirm('Did you like javascript')
+console.log(a)
+console.warn(a)
+console.error(a)
+
+
+const y=prompt('you r name')
+document.writeln(y)
+console.log(y)
+
+
+let t=prompt('your age')
+alert('30')
+console.log(t)
+document.writeln(t)
+
+{
+    var a=10
+}
+console.log(a)
+{
+    let q=20
+}
+
+console.log(q)
+
+
+
+
+
+
+
